a2ff373f1355684f2646737721dd540aade8cc97
Dan Lucas <dan.lucas@gmail.com>
2014-02-15 13:19:14 -0500

    Adding README and a view.

---
+++ app/scripts/controllers/staff.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('barbershopApp')
+  .controller('StaffCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
